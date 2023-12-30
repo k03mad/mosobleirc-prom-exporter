a43fe019e59fea2fe5623d3dbe5f1672a5a1760b
Kirill Molchanov <k03.mad@gmail.com>
2023-12-30 12:09:40 +0300

    Update date.js

--- app/helpers/date.js
+++ app/helpers/date.js
@@ -5,7 +5,7 @@ export const getDateYMD = init => {
     const date = init ? new Date(init) : new Date();
 
     const year = date.getFullYear();
-    const month = date.getMonth();
+    const month = date.getMonth() + 1;
     const day = date.getDate();
 
     return [
