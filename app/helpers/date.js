6da7adacc1048449b0367a0cccfd6976ce5f3a1f
k03mad <k03.mad@gmail.com>
2023-12-31 12:46:05 +0300

    update date

--- app/helpers/date.js
+++ app/helpers/date.js
@@ -3,29 +3,35 @@ const prefixZero = datePart => String(datePart).length === 1
     : datePart;
 
 /**
- * @param {string} [init]
+ * @param {number|string} [init]
+ * @param {object} [opts]
+ * @param {string} [opts.ymdSeparator]
  */
-export const getDateYMD = init => {
+export const getDateYMD = (init, {ymdSeparator = '-'} = {}) => {
     const date = init ? new Date(init) : new Date();
 
     return [
         date.getFullYear(),
         date.getMonth() + 1,
         date.getDate(),
-    ].map(elem => prefixZero(elem)).join('-');
+    ].map(elem => prefixZero(elem)).join(ymdSeparator);
 };
 
 /**
  * @param {string} [init]
+ * @param {object} [opts]
+ * @param {string} [opts.ymdSeparator]
+ * @param {string} [opts.hmsSeparator]
  */
-export const getDateYMDHMS = init => {
+export const getDateYMDHMS = (init, {ymdSeparator = '-', hmsSeparator = ':'} = {}) => {
     const date = init ? new Date(init) : new Date();
+    const ymd = getDateYMD(init, {ymdSeparator});
 
     const hms = [
         date.getHours(),
         date.getMinutes(),
         date.getSeconds(),
-    ].map(elem => prefixZero(elem)).join(':');
+    ].map(elem => prefixZero(elem)).join(hmsSeparator);
 
-    return `${getDateYMD(init)} ${hms}`;
+    return `${ymd} ${hms}`;
 };
